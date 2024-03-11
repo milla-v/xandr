47bd1457590dc72b7894a8bf20da8bf53727e6fd
Ludmila Voilokova <ludmila.voilokova@gmail.com>
2024-03-11 10:19:04 -0400

    Open files

--- bss/textwriter.go
+++ bss/textwriter.go
@@ -1,7 +1,9 @@
 package bss
 
 import (
+	"bufio"
 	"io"
+	"os"
 
 	"github.com/milla-v/xandr/bss/xgen"
 )
@@ -9,17 +11,35 @@ import (
 // TextFileWriter provides writing user-segments data to a file according Legacy BSS file format
 // described on https://learn.microsoft.com/en-us/xandr/bidders/legacy-bss-file-format
 type TextFileWriter struct {
-	w         io.WriteCloser
+	file      io.Closer
+	w         *bufio.Writer
 	formatter *xgen.TextFormatter
 }
 
-func NewTextFileWriter(fname string, p TextFileParameters) *TextFileFormatter {
-	return &TextFileWriter{}
+func NewTextFileWriter(fname string, p TextFileParameters) (*TextFileFormatter, error) {
+	createdFile, err := os.Create(fname)
+	if err != nil {
+		return err
+	}
+
+	tw := &TextFileWriter{
+		w:    bufio.NewWriter(createdFile),
+		file: createdFile,
+	}
+
+	return tw, nil
 }
 
-func (w *TextFileWriter) Close() error {
+func (tw *TextFileWriter) Close() error {
+	if err := tw.w.Flush(); err != nil {
+		return err
+	}
+	if err := tw.file.Close(); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (w *TextFileWriter) Append(ur *UserRecord) error {
+
 }
