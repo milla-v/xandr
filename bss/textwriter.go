e988aa6637f7f5cde9758d1e8dc717242a482ec3
Ludmila Voilokova <ludmila.voilokova@gmail.com>
2024-04-12 07:03:55 -0400

    Add comments

--- bss/textwriter.go
+++ bss/textwriter.go
@@ -7,13 +7,14 @@ import (
 	"github.com/milla-v/xandr/bss/xgen"
 )
 
-// TextFileWriter provides writing user-segments data to a file according Legacy BSS file format
+// TextFileWriter provides writing user-segments data to a writer stream according Legacy BSS file format
 // described on https://learn.microsoft.com/en-us/xandr/bidders/legacy-bss-file-format
 type TextFileWriter struct {
 	w       *bufio.Writer
 	encoder *xgen.TextEncoder
 }
 
+// NewTextFileWriter creates new BSS text writer.
 func NewTextFileWriter(w io.Writer, params xgen.TextEncoderParameters) (*TextFileWriter, error) {
 	tw := &TextFileWriter{
 		w: bufio.NewWriter(w),
@@ -29,6 +30,7 @@ func NewTextFileWriter(w io.Writer, params xgen.TextEncoderParameters) (*TextFil
 	return tw, nil
 }
 
+// Close flushes buffered text to the writer.
 func (tw *TextFileWriter) Close() error {
 	if err := tw.w.Flush(); err != nil {
 		return err
@@ -36,6 +38,7 @@ func (tw *TextFileWriter) Close() error {
 	return nil
 }
 
+// Append outputs new user record according text format parameters.
 func (w *TextFileWriter) Append(ur *xgen.UserRecord) error {
 	line, err := w.encoder.FormatLine(ur)
 	if err != nil {
